0245f3b1863e3dce5799f78b54153c62d9d32df5
cfredberg <53453343+cfredberg@users.noreply.github.com>
2021-09-22 16:55:21 -0700

    introtostack, textundoredo

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -16,10 +16,25 @@ public class _01_IntroToStack {
     		stack.push(rand.nextDouble()*100);
     	}
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-
+    	String numS = JOptionPane.showInputDialog("Please enter a number from 1 to 100 inclusive.");
+    	int num1 = Integer.parseInt(numS);
+    	numS = JOptionPane.showInputDialog("Please enter another number from 1 to 100 inclusive.");
+    	int num2 = Integer.parseInt(numS);
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, print it to the screen.
-
+    	for (int i = 0; i < 100; i++) {
+    		if (num1 < num2) {
+    			double num = stack.pop();
+    			if (num > num1 && num < num2) {
+    				System.out.println(num);
+    			}
+    		}else if (num1 > num2) {
+    			double num = stack.pop();
+    			if (num < num1 && num > num2) {
+    				System.out.println(num);
+    			}
+    		}
+    	}
 
         // EXAMPLE:
         // NUM 1: 65
--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,6 +1,16 @@
 package _00_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
     /* 
      * Create a JFrame with a JPanel and a JLabel.
      * 
@@ -17,5 +27,77 @@ public class _02_TextUndoRedo {
      * the JLabel.
      */
 
+	JFrame frame = new JFrame("The Best Text Editor in the World!!! ;-)");
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	
+	Stack<String> stack = new Stack<String>();
+	
+	public static void main(String[] args) {
+		_02_TextUndoRedo method = new _02_TextUndoRedo();
+		method.run();
+	}
+	
+	public void run() {
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(400, 250));
+		frame.add(panel);
+		frame.pack();
+		
+		panel.add(label);
+		
+		frame.addKeyListener(this);
+	}
 
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	boolean ctrl = false;
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			String text = label.getText();
+			label.setText("");
+			for (int i = 0; i < text.length(); i++) {
+				if (i == text.length()-1) {
+					stack.push(text.charAt(i) + "");
+				}else {
+					label.setText(label.getText() + text.charAt(i));
+				}
+			}
+		}else if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+			
+		}else if (e.getKeyCode() == KeyEvent.VK_CONTROL) {
+			ctrl = true;
+		}else {
+			if (ctrl) {
+				if (e.getKeyCode() == KeyEvent.VK_Z) {
+					if (!stack.isEmpty()) {
+						label.setText(label.getText() + stack.pop());
+					}
+				}else if (e.getKeyCode() == KeyEvent.VK_E) {
+					int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to empty the clipboard?");
+					if (result == 0) {
+						stack.removeAllElements();
+					}
+				}
+			}else {
+				label.setText(label.getText() + e.getKeyChar());
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyCode() == KeyEvent.VK_CONTROL) {
+			ctrl = false;
+		}
+	}
 }
